7ad350fd59ed1d2a4431ab0b6ad9fef8d5a85a3d
SilentRemix <dshkhamraev@edu.hse.ru>
2021-01-14 21:37:35 +0300

    PDF function added (only for order button )

--- GradeExpertCRM/GradeExpertCRM/ViewModels/Frames/AddingDocumentViewModel.cs
+++ GradeExpertCRM/GradeExpertCRM/ViewModels/Frames/AddingDocumentViewModel.cs
@@ -1,6 +1,10 @@
 ﻿using System.Reactive;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using Avalonia.Controls;
+using RazorLight;
 using ReactiveUI;
+using SelectPdf;
 
 namespace GradeExpertCRM.ViewModels.Frames
 {
@@ -12,11 +16,45 @@ namespace GradeExpertCRM.ViewModels.Frames
 
         public ReactiveCommand<Unit, Unit> GoDocumentView { get; }
 
+        public ReactiveCommand<Unit, Unit> SavePdfCommand { get; }
+
         public AddingDocumentViewModel(IBaseWindow baseWindow, DocumentsViewModel parent)
         {
             BaseWindow = baseWindow;
             Parent = parent;
             GoDocumentView = ReactiveCommand.CreateFromTask(OpenDocumentView);
+
+            SavePdfCommand = ReactiveCommand.CreateFromTask(SavePdf);
+        }
+
+        private async Task SavePdf()
+        {
+            SaveFileDialog dialog = new SaveFileDialog();
+            dialog.Filters.Add(new FileDialogFilter { Name = "PDF", Extensions = { "pdf" } });
+            var result = await dialog.ShowAsync(new Window());
+
+            if (result != null)
+            {
+                var engine = new RazorLightEngineBuilder()
+                    .UseEmbeddedResourcesProject(System.Reflection.Assembly.GetEntryAssembly())
+                    .UseMemoryCachingProvider()
+                    .Build();
+
+                string html = await engine.CompileRenderAsync<object>("GradeExpertCRM.Resources.Templates.Template1", null);
+
+                HtmlToPdf converter = new HtmlToPdf();
+
+                int margin = 40;
+                converter.Options.MarginLeft = margin;
+                converter.Options.MarginTop = margin / 2;
+                converter.Options.MarginRight = margin;
+                converter.Options.MarginBottom = margin;
+
+                PdfDocument doc = converter.ConvertHtmlString(html);
+
+                doc.Save(result);
+                doc.Close();
+            }
         }
     }
 }
